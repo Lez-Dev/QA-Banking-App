eb8e325512c29bb8c2bbbd29a87f61c2fbed356a
Liam <eldev.kelly@gmail.com>
2024-10-28 11:16:18 +0000

    test commit

--- QA-WEEK-3/src/main/java/org/example/Main.java
+++ QA-WEEK-3/src/main/java/org/example/Main.java
@@ -5,6 +5,7 @@ import org.example.Vehicles.Car;
 
 public class Main {
     public static void main(String[] args) {
+        /*
 
         Car car1 = new Car("AB123DE", "Peugeot", "Hybrid", 2023);
         Car car2 = new Car("FH123GZ", "VW", "Petrol");
@@ -14,6 +15,9 @@ public class Main {
 
         car2.setColour("Blue");
         System.out.println(car2.toString());
+
+         */
+        System.out.println("test");
 
 
     }
